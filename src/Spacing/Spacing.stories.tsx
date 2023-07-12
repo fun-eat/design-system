8e43d7b8bbf495e51b968ac101ae9abc64502d37
Leejin-Yang <ejin1996@gmail.com>
2023-07-12 19:31:42 +0900

    feat: Spacing 컴포넌트 스토리 작성

---
+++ src/Spacing/Spacing.stories.tsx
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { PropsWithChildren } from 'react';
+
+import Spacing from './Spacing';
+
+import theme from '@styles/theme';
+
+const meta: Meta<typeof Spacing> = {
+  title: 'Spacing',
+  component: Spacing,
+  argTypes: {
+    direction: {
+      description: '여백의 방향 입니다.',
+    },
+    size: {
+      description: '여백의 크기 입니다.',
+      control: 'number',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Spacing>;
+
+const Container = ({ children, vertical }: PropsWithChildren<{ vertical?: boolean }>) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: `${vertical ? 'column' : 'row'}`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Box = () => {
+  return (
+    <div
+      style={{
+        width: '100px',
+        height: '50px',
+        backgroundColor: `${theme.colors.primary}`,
+      }}
+    />
+  );
+};
+
+export const Default: Story = {
+  render: ({ direction, size }) => (
+    <Container vertical={direction === 'vertical'}>
+      <Box />
+      <Spacing direction={direction} size={size} />
+      <Box />
+      <Spacing direction={direction} size={size} />
+      <Box />
+      <Spacing direction={direction} size={size} />
+      <Box />
+    </Container>
+  ),
+};
+
+export const Vertical: Story = {
+  render: ({ size }) => (
+    <Container vertical>
+      <Box />
+      <Spacing direction="vertical" size={size} />
+      <Box />
+      <Spacing direction="vertical" size={size} />
+      <Box />
+      <Spacing direction="vertical" size={size} />
+      <Box />
+    </Container>
+  ),
+};
+
+export const Horizontal: Story = {
+  render: ({ size }) => (
+    <Container>
+      <Box />
+      <Spacing direction="horizontal" size={size} />
+      <Box />
+      <Spacing direction="horizontal" size={size} />
+      <Box />
+      <Spacing direction="horizontal" size={size} />
+      <Box />
+    </Container>
+  ),
+};
