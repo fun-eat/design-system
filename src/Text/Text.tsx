673412fb55d3c07fa9e42739618ac3e833e74c8d
Leejin-Yang <ejin1996@gmail.com>
2023-07-13 02:29:16 +0900

    refactor: 태그에 맞게 타입 변하도록 TextProps 수정

--- src/Text/Text.tsx
+++ src/Text/Text.tsx
@@ -1,15 +1,15 @@
-import type { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
+import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 import theme from '@styles/theme';
 
 type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-type TextTypes = Extract<keyof JSX.IntrinsicElements, 'p' | 'span'>;
+type TextTypes = Extract<ElementType, 'p' | 'span'>;
 type TextAligns = 'left' | 'center' | 'right';
 type TextWeights = 'light' | 'regular' | 'bold';
 
-export interface TextProps extends PropsWithChildren<ComponentPropsWithoutRef<'p'>> {
+interface TextStyleProps {
   type?: TextTypes;
   size?: Sizes;
   weight?: TextWeights;
@@ -18,7 +18,10 @@ export interface TextProps extends PropsWithChildren<ComponentPropsWithoutRef<'p
   align?: TextAligns;
 }
 
-const Text = ({
+export type TextProps<T extends TextTypes> = TextStyleProps &
+  PropsWithChildren<ComponentPropsWithoutRef<T>> & { as?: T };
+
+const Text = <T extends TextTypes = 'p'>({
   children,
   type,
   size = 'md',
@@ -27,7 +30,7 @@ const Text = ({
   color = theme.textColors.default,
   align = 'left',
   ...props
-}: TextProps) => {
+}: TextProps<T>) => {
   return (
     <TextContainer as={type} size={size} weight={weight} lineHeight={lineHeight} color={color} align={align} {...props}>
       {children}
@@ -37,7 +40,7 @@ const Text = ({
 
 export default Text;
 
-const TextContainer = styled.p<Pick<TextProps, 'type' | 'align' | 'size' | 'lineHeight' | 'weight'>>`
+const TextContainer = styled.p<TextStyleProps>`
   font-size: ${({ theme, size }) => theme.fontSizes[size ?? 'md']};
   font-weight: ${({ theme, weight }) => theme.fontWeights[weight ?? 'regular']};
   line-height: ${({ theme, lineHeight }) => theme.lineHeights[lineHeight ?? 'md']};
