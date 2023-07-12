6b0b651d5e9dcea801fdee0acaa87c881a684765
TaeeunKim <xodms0309@naver.com>
2023-07-12 13:45:26 +0900

    chore: 스토리북에 웹팩 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -6,8 +8,27 @@ const config: StorybookConfig = {
     name: '@storybook/react-webpack5',
     options: {},
   },
+  core: {
+    builder: {
+      name: '@storybook/builder-webpack5',
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+        '@styles': path.resolve(__dirname, '../src/styles'),
+      };
+    }
+    return config;
+  },
   docs: {
-    autodocs: 'tag',
+    autodocs: true,
   },
 };
 export default config;
