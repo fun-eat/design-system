ff8c8e77064e016d4daf5a9dd1f44476aa043732
Leejin-Yang <ejin1996@gmail.com>
2023-07-14 14:29:56 +0900

    refactor: type 파일 통일

--- src/type.ts
+++
@@ -1,8 +0,0 @@
-import type { ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType } from 'react';
-
-export type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-
-export type OverridableComponentPropsWithRef<T extends ElementType, P = unknown> = P &
-  ComponentPropsWithRef<T> & { as?: T };
-export type OverridableComponentPropsWithoutRef<T extends ElementType, P = unknown> = P &
-  ComponentPropsWithoutRef<T> & { as?: T };
