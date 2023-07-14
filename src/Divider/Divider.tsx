4b85804d2c146709e2c0701ef227ed14cda3fe73
hae-on <solbi2004@naver.com>
2023-07-14 09:59:25 +0900

    docs: jsDocs 수정

--- src/Divider/Divider.tsx
+++ src/Divider/Divider.tsx
@@ -3,11 +3,11 @@ import styled, { css } from 'styled-components';
 
 export interface DividerProps extends ComponentPropsWithoutRef<'hr'> {
   /**
-   * Divider의 종류입니다. 강조의 정도에 차이가 있습니다.
+   * Divider 컴포넌트의 종류입니다. 강조의 정도 차이가 있습니다.
    */
   variant?: 'default' | 'strong' | 'disabled';
   /**
-   * Divider의 길이입니다.
+   * Divider 컴포넌트의 길이입니다.
    */
   length?: string;
 }
