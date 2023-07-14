57e6a05f2b734992931a67c1da3a7cc33affbac9
Taeeun Kim <xodms0309@naver.com>
2023-07-14 14:19:53 +0900

    feat: Badge 컴포넌트 추가 (#14)
    * feat: Size 타입 추가
    
    * feat: Badge 컴포넌트 추가
    
    * style: import 순서 변경
    
    * refactor: 컨벤션 추가로 인한 리팩토링
    
    * refactor: 스토리북의  description -> jsdoc으로 변경
    
    * feat: index.ts 파일 추가
    
    * feat: BadgeProps의 color와 textColor의 타입 내로잉
    
    * style: 스타일 순서 수정
    
    * refactor: BadgeStyleProps type 선언

---
+++ src/Badge/Badge.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Badge from './Badge';
+
+import theme from '@styles/theme';
+
+const meta: Meta<typeof Badge> = {
+  title: 'Badge',
+  component: Badge,
+  argTypes: {
+    color: {
+      control: {
+        type: 'color',
+      },
+    },
+    textColor: {
+      control: {
+        type: 'color',
+      },
+    },
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  args: {
+    color: theme.colors.black,
+    textColor: theme.colors.white,
+    size: 'sm',
+    children: '뱃지 텍스트',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const XSmall: Story = {
+  args: {
+    size: 'xs',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
+};
+
+export const XLarge: Story = {
+  args: {
+    size: 'xl',
+  },
+};
---
+++ src/Badge/Badge.tsx
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components';
+
+import type { Sizes } from '@types';
+import type { ComponentPropsWithoutRef, CSSProperties } from 'react';
+import type { RuleSet } from 'styled-components';
+
+export interface BadgeProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Badge 컴포넌트 내부 색상입니다.
+   */
+  color: CSSProperties['color'];
+  /**
+   * Badge 컴포넌트에 들어갈 텍스트의 색상입니다.
+   */
+  textColor: CSSProperties['color'];
+  /**
+   * Badge 컴포넌트에 들어갈 텍스트의 크기입니다.
+   */
+  size?: Sizes;
+}
+
+const Badge = ({ color, textColor, size = 'sm', children, ...props }: BadgeProps) => {
+  return (
+    <BadgeContainer color={color} textColor={textColor} size={size} {...props}>
+      <span>{children}</span>
+    </BadgeContainer>
+  );
+};
+
+export default Badge;
+
+type BadgeStyleProps = Pick<BadgeProps, 'color' | 'textColor' | 'size'>;
+
+const badgeSizeStyles: Record<Sizes, RuleSet<object>> = {
+  xs: css`
+    font-size: 1rem;
+  `,
+  sm: css`
+    font-size: 1.2rem;
+  `,
+  md: css`
+    font-size: 1.4rem;
+  `,
+  lg: css`
+    font-size: 1.6rem;
+  `,
+  xl: css`
+    font-size: 1.8rem;
+  `,
+};
+
+export const BadgeContainer = styled.div<BadgeStyleProps>`
+  width: fit-content;
+  padding: 8px 12px;
+  border-radius: 8px;
+  background: ${({ color }) => color};
+  color: ${({ textColor }) => textColor};
+  ${({ size }) => badgeSizeStyles[size ?? 'sm']}
+`;
