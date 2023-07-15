6663bde7aa54d22aac799cc05c1cb13518a81426
Leejin-Yang <ejin1996@gmail.com>
2023-07-15 22:03:28 +0900

    feat: Link 컴포넌트 스토리 작성

---
+++ src/Link/Link.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Link from './Link';
+
+const meta: Meta<typeof Link> = {
+  title: 'Link',
+  component: Link,
+  args: {
+    children: '외부 링크로 이동합니다.',
+    href: '#',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Link>;
+
+export const Default: Story = {};
+
+export const Block: Story = {
+  render: ({ children }) => (
+    <>
+      <Link href="#" block isExternal>
+        {children}
+      </Link>
+      <Link href="#" block>
+        {children}
+      </Link>
+    </>
+  ),
+};
