d0887d43d29dc8528596933cd5086f15d8b5804d
Leejin-Yang <ejin1996@gmail.com>
2023-07-23 20:32:44 +0900

    feat: Controlled 스토리 작성

--- src/Textarea/Textarea.stories.tsx
+++ src/Textarea/Textarea.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import type { ChangeEventHandler } from 'react';
+import { useState } from 'react';
 
 import Textarea from './Textarea';
+import Text from '../Text/Text';
 
 const meta: Meta<typeof Textarea> = {
   title: 'Textarea',
@@ -26,5 +29,25 @@ export const Default: Story = {};
 export const WithPlaceholder: Story = {
   args: {
     placeholder: '최소 20자 이상 입력해주세요.',
+  },
+};
+
+export const Controlled: Story = {
+  render: () => {
+    const [value, setValue] = useState('');
+    const maxLength = 200;
+
+    const handleInputChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+      setValue(e.currentTarget.value);
+    };
+
+    return (
+      <>
+        <Textarea value={value} onChange={handleInputChange} maxLength={maxLength} />
+        <Text>
+          작성한 길이: {value.length} / {maxLength}
+        </Text>
+      </>
+    );
   },
 };
