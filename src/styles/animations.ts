6f1b217c41e23be1e7686f098ed15c748c01b375
hae-on <solbi2004@naver.com>
2023-07-25 16:33:06 +0900

    style: prettier에 맞게 수정

--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -1,10 +1,10 @@
 import { keyframes } from 'styled-components';
 
 export const slideUp = keyframes`
-0% {
-  bottom:-300px
-}
-100% {
-  bottom: 0;
-}
+  0% {
+    bottom: -300px;
+  }
+  100% {
+    bottom: 0;
+  }
 `;
