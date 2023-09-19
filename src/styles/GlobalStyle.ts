410350a0cd465008cc4c83622d47d7adfde4926b
hae-on <solbi2004@naver.com>
2023-09-19 21:01:47 +0900

    chore: 폰트 삭제

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-import fonts from './fonts';
-
 const GlobalStyle = createGlobalStyle`
-  ${fonts}
-
   * {
     margin: 0;
     padding: 0;
