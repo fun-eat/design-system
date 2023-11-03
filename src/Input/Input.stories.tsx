6cff133940c3770085e6e375929f89d3df8b6c7d
hae-on <solbi2004@naver.com>
2023-11-03 11:27:49 +0900

    feat: input 컴포넌트 추가

---
+++ src/Input/Input.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Input from './Input';
+
+const meta: Meta<typeof Input> = {
+  title: 'Input',
+  component: Input,
+  argTypes: {
+    rightIcon: {
+      control: { type: 'boolean' },
+      mapping: { false: '', true: '🔎' },
+    },
+  },
+  args: {
+    customWidth: '300px',
+    isError: false,
+    rightIcon: false,
+    errorMessage: '',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {};
+
+export const WithPlaceholder: Story = {
+  args: {
+    placeholder: '상품 이름을 검색하세요.',
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    placeholder: '상품 이름을 검색하세요.',
+    rightIcon: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    isError: true,
+    errorMessage: '10글자 이내로 입력해주세요.',
+  },
+};
+
+export const Disabled: Story = {
+  render: () => <Input disabled />,
+};
---
+++ src/Input/index.ts
@@ -0,0 +1,5 @@
+import Input from './Input';
+
+export type { InputProps } from './Input';
+
+export default Input;
