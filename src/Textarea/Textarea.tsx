158ec7814ba595c7115929f20776d57aa3ab9190
hae-on <solbi2004@naver.com>
2023-11-09 20:47:12 +0900

    refactor: 유효성 검사 추가

--- src/Textarea/Textarea.tsx
+++ src/Textarea/Textarea.tsx
@@ -2,6 +2,10 @@ import { forwardRef } from 'react';
 import type { CSSProperties, ComponentPropsWithRef } from 'react';
 import styled from 'styled-components';
 
+import useValidator from '../hooks/useValidator';
+import theme from '../styles/theme';
+import Text from '../Text/Text';
+
 export interface TextareaProps extends ComponentPropsWithRef<'textarea'> {
   /**
    * Textarea 컴포넌트 사이즈 재조정 방향 설정입니다.
@@ -10,14 +14,33 @@ export interface TextareaProps extends ComponentPropsWithRef<'textarea'> {
 }
 
 const Textarea = ({ resize = 'both', ref, ...props }: TextareaProps) => {
+  const { value, error, handleChange } = useValidator();
+
   return (
-    <TextareaContainer ref={ref} resize={resize} autoCapitalize="off" autoCorrect="off" spellCheck="false" {...props} />
+    <>
+      <TextareaContainer
+        value={value}
+        ref={ref}
+        resize={resize}
+        error={error}
+        autoCapitalize="off"
+        autoCorrect="off"
+        spellCheck="false"
+        onChange={handleChange}
+        {...props}
+      />
+      <Text size="xs" color={theme.colors.error} aria-live="assertive">
+        {error}
+      </Text>
+    </>
   );
 };
 
 export default forwardRef(Textarea);
 
-type TextareaStyleProps = Pick<TextareaProps, 'resize'>;
+type TextareaStyleProps = Pick<TextareaProps, 'resize'> & {
+  error: string;
+};
 
 const TextareaContainer = styled.textarea<TextareaStyleProps>`
   width: 100%;
@@ -27,7 +50,7 @@ const TextareaContainer = styled.textarea<TextareaStyleProps>`
   border-radius: 0;
   line-height: 1.5;
   resize: ${({ resize }) => resize};
-  outline-color: ${({ theme }) => theme.colors.primary};
+  outline-color: ${({ error, theme }) => (error ? theme.colors.error : theme.colors.primary)};
 
   &::placeholder {
     color: ${({ theme }) => theme.textColors.disabled};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as FunEatProvider } from './FunEatProvider';
 
 export { default as theme } from './styles/theme';
 export { default as useTheme } from './hooks/useTheme';
+export { default as useSpaceValidator } from './hooks/useSpaceValidator';
 
 export { default as Badge } from './Badge';
 export { default as BottomSheet } from './BottomSheet';
